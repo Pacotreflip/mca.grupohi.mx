6c97e354b192163eb8a2187a867ca53fc28ac87e
jfesquivel <jfesquivel@grupohi.mx>
2016-12-01 22:34:50 -0600

    Vista Show para Camiones completada

--- resources/views/camiones/show.blade.php
+++ resources/views/camiones/show.blade.php
@@ -97,6 +97,36 @@
     <fieldset>
         <legend class="scheduler-border"><i class="fa fa-arrows"></i> Información de Cubicación</legend>
         <div class="form-group">
+            {!! Form::label('Ancho', 'Ancho', ['class' => 'control-label col-sm-1']) !!}
+            <div class="col-sm-2">
+                {!! Form::text('Ancho', null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            </div>
+            {!! Form::label('Largo', 'Largo', ['class' => 'control-label col-sm-1']) !!}
+            <div class="col-sm-2">
+                {!! Form::text('Largo', null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            </div>
+            {!! Form::label('Alto', 'Alto', ['class' => 'control-label col-sm-1']) !!}
+            <div class="col-sm-2">
+                {!! Form::text('Alto', null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            </div>
+            {!! Form::label('EspacioDeGato', 'Gato', ['class' => 'control-label col-sm-1']) !!}
+            <div class="col-sm-2">
+                {!! Form::text('EspacioDeGato', null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            </div>
+        </div>
+        <div class="form-group">
+            {!! Form::label('AlturaExtension', 'Extensión', ['class' => 'control-label col-sm-1']) !!}
+            <div class="col-sm-2">
+                {!! Form::text('AlturaExtension', null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            </div>
+            {!! Form::label('CubicacionReal', 'Cubicación Real', ['class' => 'control-label col-sm-1']) !!}
+            <div class="col-sm-2">
+                {!! Form::text('CubicacionReal', null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            </div>
+            {!! Form::label('CubicacionParaPago', 'Cubicación para Pago', ['class' => 'control-label col-sm-1']) !!}
+            <div class="col-sm-2">
+                {!! Form::text('CubicacionParaPago', null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            </div>
         </div>
     </fieldset>
 </div>
