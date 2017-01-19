9e3ae71382eab6a545780ffe802604ceeaaa1279
jfesquivel <jfesquivel@JFEsquivel-Linux>
2017-01-19 12:07:18 -0600

    Modificación de las vistas de Factores de Abundamiento

--- resources/views/fda/banco_material/index.blade.php
+++ resources/views/fda/banco_material/index.blade.php
@@ -8,13 +8,14 @@
 <div id="app">
     <global-errors></global-errors>
     <fda-bancomaterial inline-template>
-        <div class="table-responsive col-md-8 col-md-offset-2">
+        <section>
             <app-errors v-bind:form="form"></app-errors>
-            <div class="form-horizontal rcorners">
+            <div class="form-horizontal rcorners col-md-8 col-md-offset-2">
                 <div class="form-group">
+                    <legend class="scheduler-border text-center"><small><i class="fa fa-cubes"></i> NUEVO FACTOR DE ABUNDAMIENTO</small></legend>
                     <label class="control-label col-sm-1">Banco</label>
                     <div class="col-sm-3">
-                        <select class="form-control"  v-model="factor.IdBanco">
+                        <select class="form-control input-sm"  v-model="factor.IdBanco">
                             <option value >--SELECCIONE--</option>
                             <option v-for="banco in bancos" v-bind:value="banco.IdOrigen">
                                 @{{ banco.Descripcion }}
@@ -23,7 +24,7 @@
                     </div>
                     <label class="control-label col-sm-1">Material</label>
                     <div class="col-sm-3">
-                        <select class="form-control"  v-model="factor.IdMaterial">
+                        <select class="form-control input-sm"  v-model="factor.IdMaterial">
                             <option value >--SELECCIONE--</option>
                             <option v-for="material in materiales" v-bind:value="material.IdMaterial">
                                 @{{ material.Descripcion }}
@@ -32,7 +33,7 @@
                     </div>
                     <label class="control-label col-sm-1">FDA</label>
                     <div class="col-sm-3">
-                        <input class="form-control" step="any" type="number" v-model="factor.FactorAbundamiento">
+                        <input class="form-control input-sm" step="any" type="number" v-model="factor.FactorAbundamiento">
                     </div>
                 </div>
             </div>
@@ -42,32 +43,34 @@
                     <span v-else><i class="fa fa-save"></i> Guardar</span>
                 </button>
             </div>
-            <hr>
-            <table v-if="factores.length" class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Banco</th>
-                        <th>Material</th>
-                        <th>FDA</th>
-                        <th>Actulizar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="fda in factores">
-                        <td><small>@{{ fda.banco }}</small></td>
-                        <td><small>@{{ fda.material }}</small></td>
-                        <td>
-                            <input style="width: 100px" type="number" step="any" class="form-control input-sm" v-model="fda.FactorAbundamiento"></td>
-                        <td>
-                            <button class="btn btn-primary btn-sm" type="submit" @click="actualizar(fda)">
-                                <span v-if="fda.guardando"><i class="fa fa-spinner fa-spin"></i></span>
-                                <span v-else><i class="fa fa-save"></i></span>
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+            <div class="table-responsive col-md-8 col-md-offset-2 rcorners">
+                <legend class="scheduler-border text-center"><small><i class="fa fa-list"></i> FACTORES DE ABUNDAMIENTO</small></legend>
+                <table v-if="factores.length" class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Banco</th>
+                            <th>Material</th>
+                            <th>FDA</th>
+                            <th>Actulizar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="fda in factores">
+                            <td><small>@{{ fda.banco }}</small></td>
+                            <td><small>@{{ fda.material }}</small></td>
+                            <td>
+                                <input style="width: 100px" type="number" step="any" class="form-control input-sm" v-model="fda.FactorAbundamiento"></td>
+                            <td>
+                                <button class="btn btn-primary btn-sm" type="submit" @click="actualizar(fda)">
+                                    <span v-if="fda.guardando"><i class="fa fa-spinner fa-spin"></i></span>
+                                    <span v-else><i class="fa fa-save"></i></span>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
     </fda-bancomaterial>
 </div>
 @stop
--- resources/views/fda/material/index.blade.php
+++ resources/views/fda/material/index.blade.php
@@ -7,13 +7,14 @@
 <div id="app">
     <global-errors></global-errors>
     <fda-material inline-template>
-        <div class="table-responsive col-md-6 col-md-offset-3">
+        <section>
             <app-errors v-bind:form="form"></app-errors>
-            <div class="form-horizontal rcorners">
+            <div class="form-horizontal col-md-6 col-md-offset-3 rcorners">
+                <legend class="text-center"><small><i class="fa fa-cube"></i> NUEVO FACTOR DE ABUNDAMIENTO</small></legend>
                 <div class="form-group">
                     <label class="control-label col-sm-2">Material</label>
                     <div class="col-sm-5">
-                        <select class="form-control"  v-model="factor.IdMaterial">
+                        <select class="form-control input-sm"  v-model="factor.IdMaterial">
                             <option value >--SELECCIONE--</option>
                             <option v-for="material in materiales" v-bind:value="material.IdMaterial">
                                 @{{ material.Descripcion }}
@@ -22,41 +23,44 @@
                     </div>
                     <label class="control-label col-sm-2">FDA</label>
                     <div class="col-sm-3">
-                        <input class="form-control" step="any" type="number" v-model="factor.FactorAbundamiento">
+                        <input class="form-control input-sm" step="any" type="number" v-model="factor.FactorAbundamiento">
                     </div>
                 </div>
             </div>
-            <div class="form-group col-md-12" style="text-align: center; margin-top: 20px">
+            <div class="form-group col-md-12" style="text-align: center; margin-top: 20px; margin-bottom: 35px">
                 <button class="btn btn-success" type="submit" @click="guardar">
                     <span v-if="guardando"><i class="fa fa-spinner fa-spin"></i> Guardar</span>
                     <span v-else><i class="fa fa-save"></i> Guardar</span>
                 </button>
             </div>      
-            <hr>
-            <table v-if="factores.length" class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Material</th>
-                        <th>FDA</th>
-                        <th>Guardar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="factor in factores">
-                        <td>@{{ factor.material.Descripcion }}</td>
-                        <td>
-                            <input class="input-sm form-control" v-model="factor.FactorAbundamiento" type="number" step="any">
-                        </td>
-                        <td>
-                            <button class="btn btn-sm btn-success" type="submit" @click="actualizar(factor)">
-                                <span v-if="factor.guardando"><i class="fa fa-spinner fa-spin"></i></span>
-                                <span v-else><i class="fa fa-save"></i></span>
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>     
+            <hr class="col-md-6 col-md-offset-3">
+            <div class="table-responsive rcorners col-md-6 col-md-offset-3">
+                <legend class="text-center"><small><i class="fa fa-list"></i> FACTORES DE ABUMDAMIENTO</small></legend>
+                <table v-if="factores.length" class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Material</th>
+                            <th>FDA</th>
+                            <th>Actualizar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="factor in factores">
+                            <td>@{{ factor.material.Descripcion }}</td>
+                            <td>
+                                <input class="input-sm form-control" v-model="factor.FactorAbundamiento" type="number" step="any">
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-primary" type="submit" @click="actualizar(factor)">
+                                    <span v-if="factor.guardando"><i class="fa fa-spinner fa-spin"></i></span>
+                                    <span v-else><i class="fa fa-save"></i></span>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>    
+        </section>
     </fda-material>
 </div>
 @stop
