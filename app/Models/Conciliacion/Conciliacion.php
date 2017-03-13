b661c0561eb99afc3fd7e531f898c016b61e3098
EMartinez <emartinez@grupohi.mx>
2017-03-13 16:10:06 -0600

    Avance Conciliación

--- app/Models/Conciliacion/Conciliacion.php
+++ app/Models/Conciliacion/Conciliacion.php
@@ -10,7 +10,6 @@ use App\Models\Viaje;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
-use App\Models\Empresa;
 use Ghi\Core\Models\User;
 class Conciliacion extends Model
 {
@@ -81,5 +80,40 @@ class Conciliacion extends Model
     public function empresa() {
         return $this->belongsTo(Empresa::class, 'idempresa');
 
+    }
+    public function partidas(){
+        return $this->hasMany(ConciliacionDetalle::class, 'idconciliacion', 'idconciliacion');
+    }
+    
+    public function getVolumenAttribute(){
+         $results = DB::connection("sca")->select("select sum(Volumen) as Volumen "
+                 . "from conciliacion "
+                 . "left join conciliacion_detalle on conciliacion.idconciliacion = conciliacion_detalle.idconciliacion "
+                 . "left join viajes on conciliacion_detalle.idviaje = viajes.IdViaje where conciliacion.idconciliacion = ".$this->idconciliacion." "
+                 . "group by conciliacion.idconciliacion limit 1");
+         return $results[0]->Volumen;
+    }
+    
+    public function getImporteAttribute(){
+                 $results = DB::connection("sca")->select("select sum(Importe) as Importe "
+                 . "from conciliacion "
+                 . "left join conciliacion_detalle on conciliacion.idconciliacion = conciliacion_detalle.idconciliacion "
+                 . "left join viajes on conciliacion_detalle.idviaje = viajes.IdViaje where conciliacion.idconciliacion = ".$this->idconciliacion." "
+                 . "group by conciliacion.idconciliacion limit 1");
+         return $results[0]->Importe;
+    }
+    public function usuario(){
+        return $this->belongsTo(User::class, "IdRegistro");
+    }
+     public function getVolumenFAttribute(){
+        
+        return number_format($this->volumen, 2, ".",",");
+    }
+    public function getImporteFAttribute(){
+        
+        return number_format($this->importe, 2, ".",",");
+    }
+    public function getFechaHoraRegistroAttribute(){
+        return ucwords($this->timestamp->formatLocalized('%d %B %Y')).' ('.$this->timestamp->format("h:i:s").')';
     }
 }
