8684735ca3145469ce25120f04a3ddde347adba4
EMartinez <emartinez@grupohi.mx>
2017-03-22 11:14:56 -0600

    Guardar dato de persona que cierra y aprueba una conciliación, así como la fecha de la acción

--- app/Models/Conciliacion/Conciliacion.php
+++ app/Models/Conciliacion/Conciliacion.php
@@ -136,6 +136,8 @@ class Conciliacion extends Model
 
         try {
             $this->estado = 1;
+            $this->IdCerro = auth()->user()->idusuario;
+            $this->FechaHoraCierre = Carbon::now()->format('Y-m-d h:m:s');
             $this->save();
 
             foreach ($this->viajes() as $v) {
@@ -174,6 +176,8 @@ class Conciliacion extends Model
 
     public function aprobar() {
         $this->estado = 2;
+        $this->IdAprobo = auth()->user()->idusuario;
+        $this->FechaHoraAprobacion = Carbon::now()->format('Y-m-d h:m:s');
         $this->save();
     }
     public function getEstadoStrAttribute(){
