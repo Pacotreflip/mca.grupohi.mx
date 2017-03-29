215571e692eee95e790b752e7ea511936af1e99e
JFEsquivel <jfesquivel@grupohi.mx>
2017-03-29 12:11:59 -0600

    Sigue reporte viajes netos

--- app/Models/Transformers/ViajeNetoReporteTransformer.php
+++ app/Models/Transformers/ViajeNetoReporteTransformer.php
@@ -20,13 +20,23 @@ class ViajeNetoReporteTransformer extends AbstractTransformer
      * @return array|mixed
      */
     public function transformModel(Model $model) {
+        dd($model);
         $output = [
-            'Estatus'                    => (String) $model->Estatus,
+            '#'                          => 1,
             'Creo Primer Toque'          => User::find($model->CreoPrimerToque) ? User::find($model->CreoPrimerToque)->present()->nombreCompleto : (String) $model->CreoPrimerToque,
             'Creo Segundo Toque'         => User::find($model->Creo) ? User::find($model->Creo)->present()->nombreCompleto : (String) $model->Creo,
             'Cubicación Camión (m3)'     => $model->camion ? (String)$model->camion->CubicacionParaPago : '',
             'Cubicación Viaje Neto (m3)' => (String) $model->CubicacionCamion,
             'Cubicación Viaje (m3)'      => $model->viaje ? (String) $model->viaje->CubicacionCamion : '',
+            'Camión'                     => (String) $model->camion,
+            'Placas Camión'              => $model->camion ? $model->camion->Placas,
+            'Placas Caja'                => $model->camion ? $model->camion->Placas,
+            'Sindicato Camion'           => '',
+            'Sindicato Viaje' => ,
+            'Empresa Viaje' => ,
+            'Sindicato Conciliado' => ,
+            'Empresa Conciliado' => ,
+
         ];
         return $output;
     }
