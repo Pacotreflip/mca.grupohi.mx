492d94d563eb701ace5515dc690a2a8f7dd526db
EMartinez <emartinez@grupohi.mx>
2017-04-19 14:29:36 -0500

    Permiso para poner viajes en conflicto pagables

--- resources/views/viajes_netos/index.blade.php
+++ resources/views/viajes_netos/index.blade.php
@@ -218,7 +218,9 @@
                               </div>
                               <div class="modal-footer">
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                @if (Auth::user()->can(['poner-viajes-conflicto-pagables']))
                                 <button type="button" class="btn btn-success" @click="confirmarPonerPagable">Es Pagable</button>
+                                @endif
                               </div>
                             </div><!-- /.modal-content -->
                           </div><!-- /.modal-dialog -->
