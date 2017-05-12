510f25dd8c6bcf5d588a7c19f4346dda839577ed
JFEsquivel <jfesquivel@grupohi.mx>
2017-05-12 14:32:36 -0500

    Reactivacion de Telefonos Desactivados

--- resources/views/telefonos/index.blade.php
+++ resources/views/telefonos/index.blade.php
@@ -34,7 +34,11 @@
                      <td>
                          <a href="{{ route('telefonos.show', $telefono) }}" title="Ver" class="btn btn-xs btn-default"><i class="fa fa-eye"></i></a>
                          <a href="{{ route('telefonos.edit', $telefono) }}" title="Editar" class="btn btn-xs btn-info"><i class="fa fa-pencil"></i></a>
-                         <button type="submit" title="Eliminar" class="btn btn-xs btn-danger" onclick="eliminar_telefono({{$telefono->id}})"><i class="fa fa-remove"></i></button>
+                         @if($telefono->estatus == 1)
+                         <button type="submit" title="Desactivar" class="btn btn-xs btn-danger" onclick="desactivar_telefono({{$telefono->id}})"><i class="fa fa-remove"></i></button>
+                         @else
+                         <button type="submit" title="Activar" class="btn btn-xs btn-success" onclick="activar_telefono({{$telefono->id}})"><i class="fa fa-plus"></i></button>
+                         @endif
                      </td>
                  </tr>
              @endforeach
@@ -51,7 +55,7 @@
 @endsection
 @section('scripts')
     <script>
-        function eliminar_telefono(id) {
+        function desactivar_telefono(id) {
             var url = App.host + '/telefonos/' + id;
             var form = $('#eliminar_telefono');
 
@@ -77,6 +81,29 @@
                 $("input[name=motivo]").val(inputValue);
                 form.submit();
             });
+        }
+
+        function activar_telefono(id) {
+            var url = App.host + '/telefonos/' + id;
+            var form = $('#eliminar_telefono');
+
+            swal({
+                    title: "¡Activar Teléfono!",
+                    text: "¿Esta seguro de que deseas activar el teléfono?",
+                    type: "warning",
+                    showCancelButton: true,
+                    closeOnConfirm: false,
+                    inputPlaceholder: "Motivo de la activación.",
+                    confirmButtonText: "Si, Activar",
+                    cancelButtonText: "No, Cancelar",
+                    showLoaderOnConfirm: true
+
+                },
+                function(){
+                    form.attr("action", url);
+                    $("input[name=motivo]").val("");
+                    form.submit();
+                });
         }
     </script>
 @endsection
