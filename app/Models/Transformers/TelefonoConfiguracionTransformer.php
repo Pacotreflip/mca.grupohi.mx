2be941274d8ab60e579bff1b63e042b279e77466
JFEsquivel <jfesquivel@grupohi.mx>
2017-05-15 10:38:08 -0500

    Clases para PDF Configuración de Teléfonos e Impresoras

---
+++ app/Models/Transformers/TelefonoConfiguracionTransformer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JFEsquivel
+ * Date: 21/03/2017
+ * Time: 12:53 PM
+ */
+
+namespace App\Models\Transformers;
+
+
+use App\Models\Conciliacion\ConciliacionDetalle;
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Themsaid\Transformers\AbstractTransformer;
+use App\Models\Conciliacion\ConciliacionDetalleNoConciliado;
+class TelefonoConfiguracionTransformer extends AbstractTransformer
+{
+    public function transformModel(Model $corte)
+    {
+        $output = [
+            'id'            => $corte->id,
+            'fecha'         => $corte->fecha,
+            'estatus'       => $corte->estatus,
+            'estado'        => $corte->estado,
+            'viajes_netos'  => ViajeNetoCorteTransformer::transform($corte->viajes_netos())
+        ];
+
+        return $output;
+    }
+}
---
+++ app/PDF/PDFTelefonosImpresoras.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JFEsquivel
+ * Date: 15/05/2017
+ * Time: 10:34 AM
+ */
+
+namespace App\PDF;
+
+
+class PDFTelefonosImpresoras
+{
+
+}
